76a1a64444899fe0e56af5d97947eef6e73c3671
Wilson <garcia.m.wilson@gmail.com>
2021-06-05 11:53:55 -0400

    Adding DI framework

---
+++ app/src/main/java/com/softcube/re_reddit/application/AppConfiguration.kt
@@ -0,0 +1,10 @@
+package com.softcube.re_reddit.application
+
+/**
+ * com.softcube.re_reddit.application
+ *
+ * Created by Wilson Garcia on 6/5/21
+ * Copyright © 2021 Wilson Garcia. All rights reserved.
+ */
+object AppConfiguration {
+}
---
+++ app/src/main/java/com/softcube/re_reddit/application/ReRedditApp.kt
@@ -0,0 +1,32 @@
+package com.softcube.re_reddit.application
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+/**
+ * com.softcube.re_reddit.application
+ *
+ * Created by Wilson Garcia on 6/5/21
+ * Copyright © 2021 Wilson Garcia. All rights reserved.
+ *
+ *
+ */
+internal class ReRedditApp: Application() {
+
+	companion object {
+		lateinit var instance: ReRedditApp
+	}
+
+	override fun onCreate() {
+		super.onCreate()
+
+		startKoin {
+			androidContext(this@ReRedditApp)
+			modules(
+			)
+		}
+
+		instance = this
+	}
+}
